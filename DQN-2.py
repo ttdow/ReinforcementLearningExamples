f50f7df35cf2d2b99b1995ea85a97831b1f16da7
ttdow <travis.t.dow@gmail.com>
2023-02-19 16:27:35 -0700

    Updated custom DQN.

--- DQN-2.py
+++ DQN-2.py
@@ -16,6 +16,24 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
 
+class SkipFrame(gym.Wrapper):
+    def __init__(self, env, skip):
+        #Return only every 'skip'-th frame
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+        # Repeat action and sum reward
+        total_reward = 0.0
+        done = False
+        for i in range(self._skip):
+            # Accumulate reward and repeat the same action
+            obs, reward, done, trunc, info = self.env.step(action)
+            total_reward += reward
+            if done:
+                break
+        return obs, total_reward, done, trunc, info
+
 class GrayScaleObservation(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env)
@@ -34,41 +52,45 @@ class GrayScaleObservation(gym.ObservationWrapper):
         observation = transform(observation)
         return observation
 
-class ReplayMemory(object):
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
+class ResizeObservation(gym.ObservationWrapper):
+    def __init__(self, env, shape):
+        super().__init__(env)
+        if isinstance(shape, int):
+            self.shape = (shape, shape)
+        else:
+            self.shape = tuple(shape)
 
-    def push(self, *args):
-        self.memory.append(Transition(*args))
+        obs_shape = self.shape + self.observation_space.shape[2:]
+        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
 
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
+    def observation(self, observation):
+        transforms = T.Compose(
+            [T.Resize(self.shape), T.Normalize(0, 255)]
+        )
+        observation = transforms(observation).squeeze(0)
+        return observation
 
 class MarioNet(nn.Module):
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        width, height = input_dim
-        channels = 1
+        channels, height, width = input_dim
 
-        if width != 240:
+        if width != 84:
             raise ValueError(f"Expecting input width: 240, got {width}")
-        if height != 256:
+        if height != 84:
             raise ValueError(f"Expecting input height: 256, got {height}")
 
         self.cnn = nn.Sequential(
-            nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=8, stride=4, padding=0),
+            nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=8, stride=4),
             nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=0),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(728, 256),
+            nn.Linear(3136, 512),
             nn.ReLU(),
-            nn.Linear(256, 12),
+            nn.Linear(512, output_dim),
         )
     
     def forward(self, input):
@@ -82,46 +104,46 @@ env.reset()
 
 next_state, reward, done, trunc, info = env.step(env.action_space.sample())
 
+env = SkipFrame(env, skip=4)
 env = GrayScaleObservation(env)
-state = env.reset()
+env = ResizeObservation(env, shape=84)
+env = FrameStack(env, num_stack=4)
 
-Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+#Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
-print(env.observation_space.shape)
-model = MarioNet(env.observation_space.shape, env.action_space).float()
+# Initialize model
+model = MarioNet(env.observation_space.shape, 12).float()
 model = model.to(device='cpu')
 
-next_state, reward, done, trunc, info = env.step(env.action_space.sample())
+def first_if_tuple(x):
+    return x[0] if isinstance(x, tuple) else x
 
-#next_state = torch.Tensor(next_state.copy().astype(np.float32).transpose(), device='cpu')
-print(next_state.shape)
-action_values = model.forward(next_state)
-print(action_values.shape)
-
-'''
+# Run game loop
 episodes = 10
 for e in range(episodes):
     state = env.reset()
+    state = state[0].__array__() if isinstance(state, tuple) else state.__array__()
+    state = torch.tensor(state, device='cpu').unsqueeze(0)
 
-    # Play the hame
     while(True):
 
-        # Run agent on state
-        action = mario.act(state)
+        # Run model over current state
+        action_values = model(state)
 
-        # Agent performs action
-        next_state, reward, done, trunc, info = env.step(ation)
+        # Perform highest-value action
+        action = torch.argmax(action_values, axis=1).item()
+        next_state, reward, done, trunc, info = env.step(action)
 
         # Remember
-        mario.cache(state, next_state, action, reward, done)
 
         # Learn
-        q, loss = mario.learn()
 
         # Update state
+        #next_state = first_if_tuple(next_state).__array__()
+        #next_state = torch.tensor(next_state, device='cpu')
         state = next_state
+        state = state[0].__array__() if isinstance(state, tuple) else state.__array__()
+        state = torch.tensor(state, device='cpu').unsqueeze(0)
 
-        # Check for end of game
-        if done or info["flag_get"]:
+        if done or info['flag_get']:
             break
-'''
